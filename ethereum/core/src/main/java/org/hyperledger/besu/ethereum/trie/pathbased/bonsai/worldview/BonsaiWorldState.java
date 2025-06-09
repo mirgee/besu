9196c0b27fcd725b1df985ba7c31097b47641f34
Miroslav Kovar <miroslavkovar@protonmail.com>
2025-06-09 12:33:01 +0200

    Mark EVM reads
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/worldview/BonsaiWorldState.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/worldview/BonsaiWorldState.java
@@ -377,7 +377,7 @@ public class BonsaiWorldState extends PathBasedWorldState {
   }
 
   @Override
-  public Account get(final Address address) {
+  public Account get(final Address address, final boolean isEvmRead) {
     return getWorldStateStorage()
         .getAccount(address.addressHash())
         .map(bytes -> BonsaiAccount.fromRLP(accumulator, address, bytes, true))
