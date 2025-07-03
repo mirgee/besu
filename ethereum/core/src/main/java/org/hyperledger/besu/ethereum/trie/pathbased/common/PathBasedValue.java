5393d72bea44e4181c2bb527aa1ffe08d643fb24
Miroslav Kovar <miroslavkovar@protonmail.com>
2025-07-03 11:47:26 +0200

    WIP: Track per-transaction updates in PathBasedValue
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/common/PathBasedValue.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/common/PathBasedValue.java
@@ -16,40 +16,84 @@ package org.hyperledger.besu.ethereum.trie.pathbased.common;
 
 import org.hyperledger.besu.plugin.services.trielogs.TrieLog;
 
+import java.util.TreeMap;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+// TODO: Using TreeMap to obtain the latest updated value. This can be done differently, e.g.
+// tracking
+// the highest index separately.
+@SuppressWarnings("NonApiType")
 public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
   private T prior;
-  private T updated;
+  private final TreeMap<Integer, T> updatedMap;
   private boolean lastStepCleared;
   private boolean clearedAtLeastOnce;
   private boolean isEvmRead;
 
+  // TODO: Sometimes, PathBasedValue is being used to represent per-block change,
+  // sometimes a per-transaction change. Maybe they should be represented by different types.
+  // Workaround for now is using -1 to index per-block change.
   public PathBasedValue(final T prior, final T updated) {
     this.prior = prior;
-    this.updated = updated;
+    this.updatedMap = new TreeMap<>();
+    this.updatedMap.put(-1, updated);
     this.lastStepCleared = false;
     this.clearedAtLeastOnce = false;
     this.isEvmRead = false;
   }
 
+  // TODO: dtto
   public PathBasedValue(final T prior, final T updated, final boolean lastStepCleared) {
     this.prior = prior;
-    this.updated = updated;
+    this.updatedMap = new TreeMap<>();
+    this.updatedMap.put(-1, updated);
     this.lastStepCleared = lastStepCleared;
     this.clearedAtLeastOnce = lastStepCleared;
   }
 
-  // TODO: Unclear & dangerous, maybe use a builder
+  public PathBasedValue(final int txIndex, final T prior, final T updated) {
+    this.prior = prior;
+    this.updatedMap = new TreeMap<>();
+    this.updatedMap.put(txIndex, updated);
+    this.lastStepCleared = false;
+    this.clearedAtLeastOnce = false;
+    this.isEvmRead = false;
+  }
+
   public PathBasedValue(
+      final int txIndex, final T prior, final T updated, final boolean lastStepCleared) {
+    this.prior = prior;
+    this.updatedMap = new TreeMap<>();
+    this.updatedMap.put(txIndex, updated);
+    this.lastStepCleared = lastStepCleared;
+    this.clearedAtLeastOnce = lastStepCleared;
+  }
+
+  public PathBasedValue(
+      final int txIndex,
       final T prior,
       final T updated,
       final boolean lastStepCleared,
       final boolean clearedAtLeastOnce,
       final boolean isEvmRead) {
     this.prior = prior;
-    this.updated = updated;
+    this.updatedMap = new TreeMap<>();
+    this.updatedMap.put(txIndex, updated);
+    this.lastStepCleared = lastStepCleared;
+    this.clearedAtLeastOnce = clearedAtLeastOnce;
+    this.isEvmRead = isEvmRead;
+  }
+
+  private PathBasedValue(
+      final T prior,
+      final TreeMap<Integer, T> updatedMap,
+      final boolean lastStepCleared,
+      final boolean clearedAtLeastOnce,
+      final boolean isEvmRead) {
+    this.prior = prior;
+    this.updatedMap = updatedMap;
     this.lastStepCleared = lastStepCleared;
     this.clearedAtLeastOnce = clearedAtLeastOnce;
     this.isEvmRead = isEvmRead;
@@ -62,7 +106,12 @@ public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
 
   @Override
   public T getUpdated() {
-    return updated;
+    return updatedMap.get(updatedMap.lastKey());
+  }
+
+  // TODO: Reconsider. Probably no need to expose the entire map
+  public TreeMap<Integer, T> getUpdatedMap() {
+    return updatedMap;
   }
 
   public PathBasedValue<T> setPrior(final T prior) {
@@ -70,12 +119,12 @@ public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
     return this;
   }
 
-  public PathBasedValue<T> setUpdated(final T updated) {
+  public PathBasedValue<T> setUpdated(final int txIndex, final T updated) {
     this.lastStepCleared = updated == null;
     if (lastStepCleared) {
       this.clearedAtLeastOnce = true;
     }
-    this.updated = updated;
+    this.updatedMap.put(txIndex, updated);
     return this;
   }
 
@@ -107,8 +156,8 @@ public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
     return "PathBasedValue{"
         + "prior="
         + prior
-        + ", updated="
-        + updated
+        + ", updatedMap="
+        + updatedMap
         + ", cleared="
         + lastStepCleared
         + ", isEvmRead="
@@ -128,7 +177,7 @@ public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
     return new EqualsBuilder()
         .append(lastStepCleared, that.lastStepCleared)
         .append(prior, that.prior)
-        .append(updated, that.updated)
+        .append(updatedMap, that.updatedMap)
         .append(isEvmRead, that.isEvmRead)
         .isEquals();
   }
@@ -137,12 +186,16 @@ public class PathBasedValue<T> implements TrieLog.LogTuple<T> {
   public int hashCode() {
     return new HashCodeBuilder(17, 37)
         .append(prior)
-        .append(updated)
+        .append(updatedMap)
         .append(lastStepCleared)
         .toHashCode();
   }
 
+  public void importUpdatesFromSource(final PathBasedValue<T> source) {
+    this.updatedMap.putAll(source.getUpdatedMap());
+  }
+
   public PathBasedValue<T> copy() {
-    return new PathBasedValue<T>(prior, updated, lastStepCleared, clearedAtLeastOnce, isEvmRead);
+    return new PathBasedValue<T>(prior, updatedMap, lastStepCleared, clearedAtLeastOnce, isEvmRead);
   }
 }
